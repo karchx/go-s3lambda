0d9d0aa37f14516cceda12312bb2d7427532b0f4
Стив <kenethra.74@gmail.com>
2023-09-13 21:41:37 -0600

    feat: test another lambda

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+type Even struct {
+	Number *int `json:"number"`
+}
+
+type EvenResponse struct {
+	IsEven bool `json:"is_event"`
+}
 
 func response(stringResponse string, statusCode int) events.APIGatewayProxyResponse {
 	return events.APIGatewayProxyResponse{Body: stringResponse, StatusCode: statusCode}
@@ -20,9 +30,35 @@ func ResponseServerError(stringResponse string) events.APIGatewayProxyResponse {
 }
 
 func HandleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-  log.Println(req)
+	log.Println(req)
+	if req.Body == "" {
+		return events.APIGatewayProxyResponse{}, errors.New("empty body")
+	}
 
-  return ResponseOK("ready!"), nil
+	var body Even
+	err := json.Unmarshal([]byte(req.Body), &body)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	var check bool
+
+	if *body.Number%2 == 0 {
+		check = true
+	} else {
+		check = false
+	}
+
+	res := EvenResponse{
+		IsEven: check,
+	}
+
+	bytesRes, err := json.Marshal(res)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	return ResponseOK(string(bytesRes)), nil
 }
 
 func main() {
